f0cf15aee5e377a66d923cec86c7ec22bec751c8
Ahmed Hussein <hsino105@gmail.com>
2025-09-27 12:51:48 +0300

    Add launch screen assets and flavor support for the app
    - Created launch images for staging, production, and development environments.
    - Added storyboard files for launch screens corresponding to each environment.
    - Implemented flavor management with an enum to differentiate between dev, staging, and prod.
    - Updated the main app widget to display a banner indicating the current flavor.
    - Created a home page that displays a greeting based on the current flavor.

---
+++ lib/flavors.dart
@@ -0,0 +1,23 @@
+enum Flavor {
+  dev,
+  staging,
+  prod,
+}
+
+class F {
+  static late final Flavor appFlavor;
+
+  static String get name => appFlavor.name;
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.dev:
+        return '[Dev] ScalableCI|CD';
+      case Flavor.staging:
+        return '[Staging] ScalableCI|CD';
+      case Flavor.prod:
+        return 'ScalableCI|CD';
+    }
+  }
+
+}
