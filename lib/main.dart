68924cb7f362fe1544e7114fd0c399bde22bd6b5
Ahmed Hussein <hsino105@gmail.com>
2025-10-07 13:46:02 +0300

    Integrate Sentry for error tracking, update dependencies in pubspec.yaml and Podfile, and enhance iOS project configuration; add error handling in main.dart and my_home_page.dart for production environment.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
   );
 
-  runApp(const App());
+  if (F.appFlavor == Flavor.prod) {
+    runZonedGuarded(
+      () async {
+        await SentryFlutter.init((options) {
+          options.dsn =
+              'https://f803c7a1427bb7b0c275463061ad369a@o4510025900163072.ingest.de.sentry.io/4510147704062032';
+          options.sendDefaultPii = true;
+        });
+
+        runApp(App());
+      },
+      (exception, stackTrace) async {
+        await Sentry.captureException(exception, stackTrace: stackTrace);
+      },
+    );
+  } else {
+    runApp(const App());
+  }
 }
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -9,6 +10,22 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(F.title)),
       body: Center(child: Text('Hello ${F.title}')),
+      floatingActionButton: F.appFlavor == Flavor.prod
+          ? FloatingActionButton(
+              onPressed: () async {
+                try {
+                  throw StateError('Sentry Test Exception');
+                } catch (exception, stackTrace) {
+                  await Sentry.captureException(
+                    exception,
+                    stackTrace: stackTrace,
+                  );
+                }
+              },
+              tooltip: 'Debug',
+              child: const Icon(Icons.bug_report),
+            )
+          : null,
     );
   }
 }
