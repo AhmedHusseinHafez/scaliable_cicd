1bcc5612f565239de60490e83e5a9eb63cbd69aa
AhmedHussein <113314000+AhmedHusseinHafez@users.noreply.github.com>
2025-12-01 22:03:00 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -9,6 +9,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: F.title,
       theme: ThemeData(primarySwatch: Colors.blue),
